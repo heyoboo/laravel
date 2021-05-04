dc7f4d39daaf03c0a1700fc7ae11207d18cecef2
heyoboo <heyosister@gmail.com>
2021-05-04 14:19:08 +0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,30 @@ Route::get('/', function () {
 
 // creat custom route // open on url - /test
 route::get('test',function(){
+    
+    // variable declaration
+    $data = 'abc';
+    $hehe = 123;
+    $hmm = 123.123;
+    $no = true;
+    $stuff = [1,2,3];
+    // obj structure
+    $obj = ['a'=>123, 'b'=>'lmao'];
+
+    $num1 = 5;
+    $num2 = 2;
+
+    // print
+    dd($data,
+    $hehe,
+    $hmm,
+    $no,
+    $stuff,
+    $obj['a'],
+    $num1 + $num2,
+    $num1 . $num2);
+
+    
     return 'hello world hhahahahahhhahahha';
 });
 
